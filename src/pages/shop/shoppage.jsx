6504aaf4635b32de0086ec78172657fa6c6c7738
Shivesh Dewangan <shivesh.dewangan@gmail.com>
2019-07-21 15:07:18 +1000

    updated directory component and collections overview component

--- src/pages/shop/shoppage.jsx
+++ src/pages/shop/shoppage.jsx
@@ -1,21 +1,10 @@
-import React, { Component } from "react";
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/collection-preview/collection-preview";
+import React from "react";
+import CollectionsOverview from "../../components/collections-overview/collections-overview";
 
-class ShopPage extends Component {
-  state = {
-    collections: SHOP_DATA
-  };
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = () => (
+  <div className='shop-page'>
+    <CollectionsOverview />
+  </div>
+);
 
 export default ShopPage;
